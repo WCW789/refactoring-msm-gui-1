256550f22fd97be1fe64bba8f8fcaaa95a8bc25d
Winnie Wu <32531807+WCW789@users.noreply.github.com>
2024-02-16 17:01:03 +0000

    pushing up code

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,15 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    my_movie_id = self.movie_id
+    matching_movies = Movie.where({ :id => my_movie_id })
+    return matching_movies.at(0)
+  end
+
+  def actor
+    my_actor_id = self.actor_id
+    matching_actor = Actor.where({ :id => my_actor_id })
+    return matching_actor.at(0)
+  end
 end
